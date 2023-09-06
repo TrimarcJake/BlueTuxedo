4dda1e2edb77603aab0c286657b73265b868bc23
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-06 06:44:12 -0700

    +SPNs>Computers, +Users, +Users>DnsAdmins

--- Tests/Build-BlueTuxedoLab.ps1
+++ Tests/Build-BlueTuxedoLab.ps1
@@ -6,16 +6,25 @@ $Domains = (Get-ADForest).Domains
 
 # Create Stuff in Each Domain
 $i = 0
-foreach($domain in $Domains) {
+foreach($domain in $Domains) {    
     # Get Domain's DN
     $DomainRoot = (Get-ADDomain $domain).distinguishedName
 
     # Create New OU For Lab Objects
-    New-ADOrganizationalUnit -Name 'ADIDNSLab' -Path $DomainRoot -Server $domain
+    New-ADOrganizationalUnit -Name 'BlueTuxedo' -Path $DomainRoot -Server $domain -ProtectedFromAccidentalDeletion $False
 
     # Create Computer Object
-    New-ADComputer -Name "DSPN$i" -SAMAccountName "DSPN$i" -Path "OU=ADIDNSLab,$DomainRoot" -Server $domain
+    New-ADComputer -Name "BlueTuxedoDSPN$i" -SAMAccountName "BlueTuxedoDSPN$i" -Path "OU=BlueTuxedo,$DomainRoot" -Server $domain
 
+    # Assign Custom SPNs to Computer Object
+    setspn -s "BlueTuxedo/BlueTuxedoDSPN$i" "$domain\BlueTuxedoDSPN$i"
+    setspn -s "BlueTuxedo/BlueTuxedoDSPN$i.$domain" "$domain\BlueTuxedoDSPN$i"
+
+    # Create New User
+    New-ADUser -Name "BlueTuxedoDnsAdmins$i" -SamAccountName "BlueTuxedoDnsAdmins$i" -Path "OU=BlueTuxedo,$DomainRoot" -Server $domain
+
+    # Add New User to DnsAdmins
+    Add-ADGroupMember -Identity 'DnsAdmins' -Members "BlueTuxedoDnsAdmins$i" -Server $domain
+    
     $i++
 }
-
