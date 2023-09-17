20edebafcc90dc010c3ce8e9e87e4bfce310a227
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-17 07:42:23 -0500

    Improved logic. Good enough for now. Works standalone.

--- Private/Get-DanglingSPN.ps1
+++ Private/Get-DanglingSPN.ps1
@@ -1,3 +1,57 @@
-function Get-DanglingSPN {
-    
+$Domains = (Get-ADForest).Domains
+$danglingSPNList = @()
+
+foreach ($domain in $Domains) {
+
+# function Get-DanglingSPN {
+    # Get all objects w/SPNs
+    $PrincipalWithSPN = Get-ADObject -Filter { ServicePrincipalName -ne "$null" -and ServicePrincipalName -ne 'kadmin/changepw' } -Properties * -Server $domain
+
+    foreach ($principal in $PrincipalWithSPN) {
+        $spnHostList = @()
+
+        # Get SPN hostname and check if DNS record exists
+        foreach ($spn in $principal.ServicePrincipalName) {
+
+            # Get SPN hostname
+            $spnHost = $spn.Split('/')[1]
+
+            # Regex filters out bare GUIDs. Intended for DCs, but need to add additional DC OU identification 
+            if ($spnHost -notmatch '^[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?$') {  
+                
+                # Check if DNS record exists for SPN hostname
+                $RRTypes = 'A', 'AAAA', 'TXT', 'CNAME'
+                $dnsResourceRecordExist = $false
+                $hostnameResolves = $false
+                foreach ($rrtype in $RRTypes) {
+                    if (Get-DnsServerResourceRecord -ComputerName $domain -ZoneName $domain -RRType $rrtype -Name $spnHost -ErrorAction Ignore) {
+                        $dnsResourceRecordExist = $true
+                    }
+
+                    if (Resolve-DnsName -Name $spnHost -Type $rrtype -ErrorAction Ignore) {
+                        $hostnameResolves = $true
+                    }
+
+                    if ( $dnsResourceRecordExist -or $hostnameResolves ) {
+                        "SPN hostname $spnHost is valid"
+                    } else {
+                        "Dangling SPN $spnHost exists on $($principal.Name)"
+                        $danglingSPN = [PSCustomObject]@{
+                            Name  = $principal.Name
+                            'Distinguished Name' = $principal.distinguishedName
+                            'Dangling SPN' = $spn
+                        }
+
+                        if ( ($danglingSPNList.'Dangling SPN' -notcontains $danglingSPN.'Dangling SPN') ) {
+                            $danglingSPNList += $danglingSPN
+                        }
+                    }
+                }
+            }
+        }
+    }
+#}
+
 }
+
+$danglingSPNList
