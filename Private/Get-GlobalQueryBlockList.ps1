8cf875f55bce3496b352cee727c0bf7d622ba9ff
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-23 05:57:54 -0500

    Initial Commit.

---
+++ Private/Get-GlobalQueryBlockList.ps1
@@ -0,0 +1,28 @@
+function Get-GlobalQueryBlockList {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [array]
+        $Domains
+    )
+
+    $GlobalQueryBlockListList = @()
+    foreach ($domain in $Domains) {
+        $DNSServers = Resolve-DnsName -Type NS -Name $domain | Where-Object QueryType -eq 'A'
+        foreach ($dnsServer in $DNSServers) {
+            [array]$GlobalQueryBlockList = Get-DnsServerGlobalQueryBlockList -ComputerName $dnsServer.IP4Address
+            Write-Host "GQBL: $($GlobalQueryBlockList.List)";pause
+            if ($GlobalQueryBlockListList.'Server IP' -notcontains $dnsServer.IP4Address) {
+                $AddToList = [PSCustomObject]@{
+                    'Server Name'   = $dnsServer.Name
+                    'Server IP'     = $dnsServer.IP4Address
+                    GQBL    = $GlobalQueryBlockList.List
+                }
+            }
+
+            $GlobalQueryBlockListList += $AddToList
+        }
+    }
+
+    $GlobalQueryBlockListList
+}
