7cd82a18a514000a00e3dc467e6bb108e0e3a6e1
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-23 08:24:04 -0500

    Initial commit.

---
+++ Private/Get-ConditionalForwarder.ps1
@@ -0,0 +1,31 @@
+function Get-ConditionalForwarder {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [array]
+        $Domains
+    )
+
+    $ZoneList = @()
+    foreach ($domain in $Domains) {
+        $DNSServers = Resolve-DnsName -Type NS -Name $domain | Where-Object QueryType -eq 'A'
+        foreach ($dnsServer in $DNSServers) {
+            $Zones = Get-DnsServerZone -ComputerName $dnsServer.IP4Address | Where-Object { 
+                ($_.IsAutoCreated -eq $false) -and 
+                ($_.ZoneType -eq 'Forwarder') -and
+                ($_.IsDsIntegrated -eq $true)
+            }
+            
+            foreach ($zone in $Zones) {
+                $AddToList = [PSCustomObject]@{
+                    'Domain' = $domain
+                    'Zone Name'   = $zone.ZoneName
+                }
+                
+                $ZoneList += $AddToList
+            }
+        }
+    }
+
+    $ZoneList
+}
