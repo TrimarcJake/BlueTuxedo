3bb26334e789363e12521e5b49af6dd6bba6874e
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-23 05:58:32 -0500

    Improved deduplication

--- Private/Get-ForwarderConfiguration.ps1
+++ Private/Get-ForwarderConfiguration.ps1
@@ -12,7 +12,7 @@ function Get-ForwarderConfiguration {
         foreach ($dnsServer in $DNSServers) {
             [array]$Forwarders = Get-DnsServerForwarder -ComputerName $dnsServer.IP4Address
             foreach ($forwarder in $Forwarders) {
-                if ($ForwarderList.'Server IP' -notcontains $forwarder.IPAddress.IPAddressToString) {
+                if ($ForwarderList.'Server IP' -notcontains $dnsServer.IPAddress.IPAddressToString) {
                     $AddToList = [PSCustomObject]@{
                         'Server Name'   = $dnsServer.Name
                         'Server IP'     = $dnsServer.IP4Address
