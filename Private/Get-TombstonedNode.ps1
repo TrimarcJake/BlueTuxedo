ae17f7ba5549427a098b646a8ac78c1e183d5fcf
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-23 11:17:46 -0500

    Initial commit

---
+++ Private/Get-TombstonedNode.ps1
@@ -0,0 +1,34 @@
+function Get-TombstonedNode {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [array]
+        $Domains
+    )
+
+    $TombstonedNodeList = @()
+    foreach ($domain in $Domains) {
+        $domainDN = (Get-ADDomain $domain).DistinguishedName
+        $Zones = Get-DnsServerZone -ComputerName $domain
+        foreach ($zone in $Zones) {
+            $Nodes = Get-DnsServerResourceRecord -ComputerName $domain -ZoneName $zone.ZoneName
+            foreach ($node in $Nodes) {
+                if ($node.DistinguishedName -like "*$domainDN") {
+                    $nodeDetails = Get-ADObject -Identity $node.DistinguishedName -Properties Name, dNSTombstoned, DistinguishedName -Server $domain
+                }
+                if ($nodeDetails.dNSTombstoned) {
+                    $AddToList = [PSCustomObject]@{
+                        'Zone Name'   = $zone.ZoneName
+                        'Node Name'   = $node.HostName
+                        'Record Type' = $node.RecordType
+                        'Node DN'     = $node.DistinguishedName
+                    }
+                
+                    $TombstonedNodeList += $AddToList
+                }
+            }
+        }
+    }
+
+    $TombstonedNodeList
+}
