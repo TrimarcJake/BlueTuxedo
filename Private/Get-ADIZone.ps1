92fd10a09db21584ab35b9df8a8ef6a41499d723
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-23 09:48:21 -0500

    Improved logic

--- Private/Get-ADIZone.ps1
+++ Private/Get-ADIZone.ps1
@@ -8,24 +8,21 @@ function Get-ADIZone {
 
     $ZoneList = @()
     foreach ($domain in $Domains) {
-        $DNSServers = Resolve-DnsName -Type NS -Name $domain | Where-Object QueryType -eq 'A'
-        foreach ($dnsServer in $DNSServers) {
-            $Zones = Get-DnsServerZone -ComputerName $dnsServer.IP4Address | Where-Object { 
-                ($_.IsAutoCreated -eq $false) -and 
-                ($_.ZoneType -ne 'Forwarder') -and
-                ($_.IsDsIntegrated -eq $true)
+        $Zones = Get-DnsServerZone -ComputerName $domain | Where-Object { 
+            ($_.IsAutoCreated -eq $false) -and 
+            ($_.ZoneType -ne 'Forwarder') -and
+            ($_.IsDsIntegrated -eq $true)
+        }
+        
+        foreach ($zone in $Zones) {
+            $AddToList = [PSCustomObject]@{
+                'Domain' = $domain
+                'Zone Name'   = $zone.ZoneName
+                'Zone Type'   = $zone.ZoneType
+                'Is Reverse?' = $zone.IsReverseLookupZone
             }
             
-            foreach ($zone in $Zones) {
-                $AddToList = [PSCustomObject]@{
-                    'Domain' = $domain
-                    'Zone Name'   = $zone.ZoneName
-                    'Zone Type'   = $zone.ZoneType
-                    'Is Reverse?' = $zone.IsReverseLookupZone
-                }
-                
-                $ZoneList += $AddToList
-            }
+            $ZoneList += $AddToList
         }
     }
 
