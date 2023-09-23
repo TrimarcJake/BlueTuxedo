68bd19bed34d9d057ce1a6142cd453c11ff6894f
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-23 06:44:14 -0500

    Initial commit.

---
+++ Private/Get-QueryResolutionPolicy.ps1
@@ -0,0 +1,34 @@
+function Get-QueryResolutionPolicy {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [array]
+        $Domains
+    )
+
+    $QueryResolutionPolicyList = @()
+    foreach ($domain in $Domains) {
+        $DNSServers = Resolve-DnsName -Type NS -Name $domain | Where-Object QueryType -eq 'A'
+        foreach ($dnsServer in $DNSServers) {
+            $QueryResolutionPolicies = (Get-DnsServer -ComputerName $dnsServer.IP4Address -ErrorAction Ignore -WarningAction Ignore).ServerPolicies
+            if ($QueryResolutionPolicyList.'Server IP' -notcontains $dnsServer.IP4Address) {
+                    foreach ($policy in $QueryResolutionPolicies) {
+                        $AddToList = [PSCustomObject]@{
+                        'Server Name'          = $dnsServer.Name
+                        'Server IP'            = $dnsServer.IP4Address
+                        'QRP Name'             = $policy.Name
+                        'QRP Level'            = $policy.Level
+                        'QRP Processing Order' = $policy.ProcessingOrder
+                        'QRP Enabled?'         = $policy.IsEnabled
+                        'QRP Action'           = $policy.Action
+                    }
+
+                    $QueryResolutionPolicyList += $AddToList
+                }
+            }
+
+        }
+    }
+
+    $QueryResolutionPolicyList
+}
