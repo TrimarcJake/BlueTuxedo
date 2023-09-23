8eb637e90e0b172305b4c630fb0dd456ae58f361
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-23 11:38:00 -0500

    Adding all private functions into public

--- Public/Invoke-BlueTuxedo.ps1
+++ Public/Invoke-BlueTuxedo.ps1
@@ -3,5 +3,19 @@ function Invoke-BlueTuxedo {
     #     $Forest
     # )
     $Domains = (Get-ADForest).Domains
-    Get-DanglingSPN $Domains
+    Get-ADIZone -Domains $Domains
+    Get-ConditionalForwarder -Domains $Domains
+    # Get-DACL -Domains $Domains
+    Get-DanglingSPN -Domains $Domains
+    Get-DnsAdminsMembership -Domains $Domains
+    Get-DynamicUpdateServiceAccount -Domains $Domains
+    Get-ForwarderConfiguration -Domains $Domains
+    Get-GlobalQueryBlockList -Domains $Domains
+    Get-NonADIZone -Domains $Domains
+    Get-QueryResolutionPolicy -Domains $Domains
+    Get-SocketPoolSize -Domains $Domains
+    Get-TombstonedNode -Domains $Domains
+    Get-WildcardRecord -Domains $Domains
+    Get-WPADRecord -Domains $Domains
+    Get-ZoneScope -Domains $Domains
 }
