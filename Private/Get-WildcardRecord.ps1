08ebc61f1219cfa588d965ef2d12b214fc0d0b51
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-23 09:05:03 -0500

    Initial commit.

---
+++ Private/Get-WildcardRecord.ps1
@@ -0,0 +1,40 @@
+function Get-WildcardRecord {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [array]
+        $Domains
+    )
+
+    $WildcardRecordList = @()
+    foreach ($domain in $Domains) {
+        $RRTypes = @('HInfo','Afsdb','Atma','Isdn','Key','Mb','Md','Mf','Mg','MInfo','Mr','Mx','NsNxt','Rp','Rt','Wks','X25','A',
+        'AAAA','CName','Ptr','Srv','Txt','Wins','WinsR','Ns','Soa','NasP','NasPtr','DName','Gpos','Loc','DhcId','Naptr','RRSig',
+        'DnsKey','DS','NSec','NSec3','NSec3Param','Tlsa')
+        $WildcardExists = $false
+        foreach ($rrtype in $RRTypes) {
+            if (Get-DnsServerResourceRecord -ComputerName $domain -ZoneName $domain -RRType $rrtype -Name '*' -ErrorAction Ignore) {
+                $WildcardExists = $true
+                $ActualRRType = $rrtype
+            }
+        }
+
+        if ($WildcardExists -eq $true) {
+            $AddToList = [PSCustomObject]@{
+                'Domain'           = $domain
+                'Wildcard Exists?' = $true
+                'Wildcard Type'    = $ActualRRType
+            } 
+        } else {
+            $AddToList = [PSCustomObject]@{
+                'Domain'           = $domain
+                'Wildcard Exists?' = $false
+                'Wildcard Type'    = 'N/A'
+            }
+        }
+        
+        $WildcardRecordList += $AddToList
+    }
+
+    $WildcardRecordList
+}
