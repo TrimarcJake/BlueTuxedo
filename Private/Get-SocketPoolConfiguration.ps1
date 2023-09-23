b7ecfc640cdb13d742dba3d38804041ed64eec2f
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-23 06:12:23 -0500

    Initial Commit

---
+++ Private/Get-SocketPoolConfiguration.ps1
@@ -0,0 +1,27 @@
+function Get-SocketPoolSize {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [array]
+        $Domains
+    )
+
+    $SocketPoolSizeList = @()
+    foreach ($domain in $Domains) {
+        $DNSServers = Resolve-DnsName -Type NS -Name $domain | Where-Object QueryType -eq 'A'
+        foreach ($dnsServer in $DNSServers) {
+            [int32]$SocketPoolSize = (Get-DnsServerSetting -ComputerName $dnsServer.IP4Address -All -WarningAction Ignore).SocketPoolSize
+            if ($SocketPoolSizeList.'Server IP' -notcontains $dnsServer.IP4Address) {
+                $AddToList = [PSCustomObject]@{
+                    'Server Name'         = $dnsServer.Name
+                    'Server IP'           = $dnsServer.IP4Address
+                    'Socket Pool Size'    = $SocketPoolSize
+                }
+            }
+
+            $SocketPoolSizeList += $AddToList
+        }
+    }
+
+    $SocketPoolSizeList
+}
