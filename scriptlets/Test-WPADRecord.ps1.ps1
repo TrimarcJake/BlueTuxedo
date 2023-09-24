2c98bcb5b69f9f91c6dcda00b94716f69d3b5d33
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-24 07:32:23 -0500

    Initial commit

---
+++ scriptlets/Test-WPADRecord.ps1.ps1
@@ -0,0 +1,28 @@
+function Test-WPADRecord {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [array]$WPADRecords
+    )
+
+    if ($WPADRecords -eq 1) {
+        $correctType = 'A'
+    } else {
+        $correctType = 'Txt'
+    }
+    $FailedWPADRecord = @()
+    foreach ($wpadrecord in $WPADRecords) {
+        if ($wpadrecord.'WPAD Type' -ne $correctType) {
+            $AddToList = [PSCustomObject]@{
+                'Domain'                = $wpadrecord.'Domain'
+                'WPAD Exists?'      = $wpadrecord.'WPAD Exists?'
+                'Current WPAD Type' = $wpadrecord.'WPAD Type'
+                'Correct Type'          = $correctType
+            }
+        }
+
+        $FailedWPADRecord += $AddToList
+    }
+
+    $FailedWPADRecord
+}
