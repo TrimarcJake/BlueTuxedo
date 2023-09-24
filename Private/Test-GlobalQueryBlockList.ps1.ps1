91e50a3293cc86546c385043416a3ae87355f324
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-09-24 07:10:36 -0500

    Initial Commit

---
+++ Private/Test-GlobalQueryBlockList.ps1.ps1
@@ -0,0 +1,30 @@
+function Test-GlobalQueryBlockList {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [array]$GlobalQueryBlockLists
+    )
+
+    $FailedGlobalQueryBlockList = @()
+    foreach ($globalqueryblocklist in $GlobalQueryBlockLists) {
+        $wpadExists = $true
+        $isatapExists = $true
+        if ($globalqueryblocklist.GQBL -notcontains 'wpad') {
+            $wpadExists = $false
+        }
+        if ($globalqueryblocklist.GQBL -notcontains 'isatap') {
+            $isatapExists = $false
+        }
+
+        $AddToList = [PSCustomObject]@{
+            'Server Name'    = $globalqueryblocklist.'Server Name'
+            'Server IP'      = $globalqueryblocklist.'Server IP'
+            'WPAD Exists'   = $wpadExists
+            'ISATAP Exists' = $isatapExists
+        }
+
+        $FailedGlobalQueryBlockList += $AddToList
+    }
+
+    $FailedGlobalQueryBlockList
+}
