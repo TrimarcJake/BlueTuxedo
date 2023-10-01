171fe6d47ffcf9acb0f64b16a2cda6707476be66
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-10-01 05:46:35 -0500

    Getting Zone Scope Containers now

--- Private/Get/Get-ZoneScopeContainer.ps1
+++ Private/Get/Get-ZoneScopeContainer.ps1
@@ -8,13 +8,17 @@ function Get-ZoneScopeContainer {
     $ZoneScopeContainerList = @()
     foreach ($adizone in $ADIZones) {
         [string]$domainDN = (Get-ADDomain $adizone.Domain).DistinguishedName
-        # try {
-            "CN=ZoneScopeContainer,DC=$($adizone.'Zone Name'),CN=MicrosoftDNS,CN=System,$domainDN"
-            $zoneScopeDN = Get-ADObject -Identity "CN=ZoneScopeContainer,DC=$($adizone.'Zone Name'),CN=MicrosoftDNS,CN=System,$domainDN" -Server $adizone.Domain -Properties DistinguishedName # -ErrorAction SilentlyContinue 
-            $zoneScopeDN
-        # } catch {
+        try {
+            $zoneScopeDN = Get-ADObject -Identity "CN=ZoneScopeContainer,DC=$($adizone.'Zone Name'),CN=MicrosoftDNS,DC=DomainDnsZones,$domainDN" -Server $adizone.Domain -Properties DistinguishedName -ErrorAction Ignore 
+            $AddToList = [PSCustomObject]@{
+                Domain                    = $adizone.Domain
+                'Zone Name'               = $adizone.'Zone Name'
+                'Zone Scope Container DN' = $zoneScopeDN
+            }
+            $ZoneScopeContainerList += $AddToList
+        } catch {
 
-        # }
+        }
     }
 
     $ZoneScopeContainerList
