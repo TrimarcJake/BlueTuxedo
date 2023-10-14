0e5b49314bea75e73c5430ec98f1971e328c702f
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-10-14 11:30:44 -0500

    Testing- and Show- now run automatically.

--- Public/Invoke-BlueTuxedo.ps1
+++ Public/Invoke-BlueTuxedo.ps1
@@ -6,19 +6,44 @@ function Invoke-BlueTuxedo {
     )
     
     $Domains = Get-Target -Forest $Forest -InputPath $InputPath
-    Get-ADIZone -Domains $Domains
-    Get-ConditionalForwarder -Domains $Domains
-    Get-DanglingSPN -Domains $Domains
-    Get-DnsAdminsMembership -Domains $Domains
-    Get-DynamicUpdateServiceAccount -Domains $Domains
-    Get-ForwarderConfiguration -Domains $Domains
-    Get-GlobalQueryBlockList -Domains $Domains
-    Get-NonADIZone -Domains $Domains
-    Get-QueryResolutionPolicy -Domains $Domains
-    Get-SecurityDescriptor -Domains $Domains
-    Get-SocketPoolSize -Domains $Domains
-    Get-TombstonedNode -Domains $Domains
-    Get-WildcardRecord -Domains $Domains
-    Get-WPADRecord -Domains $Domains
-    Get-ZoneScope -Domains $Domains
+
+    # Get Data
+    $ADIZones = Get-ADIZone -Domains $Domains
+    $ConditionalForwarders = Get-ConditionalForwarder -Domains $Domains
+    $DanglingSPNs = Get-DanglingSPN -Domains $Domains
+    $DnsAdminsMemberships = Get-DnsAdminsMembership -Domains $Domains
+    $DynamicUpdateServiceAccounts = Get-DynamicUpdateServiceAccount -Domains $Domains
+    $ForwarderConfigurations = Get-ForwarderConfiguration -Domains $Domains
+    $GlobalQueryBlockLists = Get-GlobalQueryBlockList -Domains $Domains
+    $NonADIZones = Get-NonADIZone -Domains $Domains
+    $QueryResolutionPolicys = Get-QueryResolutionPolicy -Domains $Domains
+    $SecurityDescriptors = Get-SecurityDescriptor -Domains $Domains
+    $SocketPoolSizes = Get-SocketPoolSize -Domains $Domains
+    $TombstonedNodes = Get-TombstonedNode -Domains $Domains
+    $WildcardRecords = Get-WildcardRecord -Domains $Domains
+    $WPADRecords = Get-WPADRecord -Domains $Domains
+    $ZoneScopes = Get-ZoneScope -Domains $Domains
+    $ZoneScopeContainers = Get-ZoneScopeContainer -ADIZones $ADIZones
+
+    # Test Data
+    $TestedADIZones = Test-ADIZone -ADIZones $ADIZones
+    $TestedDynamicUpdateServiceAccounts = Test-DynamicUpdateServiceAccount -DynamicUpdateServiceAccounts $DynamicUpdateServiceAccounts
+    $TestedForwarderConfigurations = Test-ForwarderConfiguration -ForwarderConfigurations $ForwarderConfigurations
+    $TestedGlobalQueryBlockLists = Test-GlobalQueryBlockList -GlobalQueryBlockLists $GlobalQueryBlockLists
+    $TestedSecurityDescriptorACEs = Test-SecurityDescriptorACE -SecurityDescriptors $SecurityDescriptors -DynamicUpdateServiceAccounts $DynamicUpdateServiceAccounts -Domains $Domains
+    $TestedSecurityDescriptorOwners = Test-SecurityDescriptorOwner -SecurityDescriptors $SecurityDescriptors -DynamicUpdateServiceAccounts $DynamicUpdateServiceAccounts -Domains $Domains
+    $TestedSocketPoolSizes = Test-SocketPoolSize -SocketPoolSizes $SocketPoolSizes 
+    $TestedWildcardRecords = Test-WildcardRecord -WildcardRecords $WildcardRecords
+    $TestedWPADRecords = Test-WPADRecord -WPADRecords $WPADRecords
+    $TestedZoneScopeContainers = Test-ZoneScopeContainer -ZoneScopeContainers $ZoneScopeContainers
+
+    # Generate Fixes
+
+    # Display All Collected Data
+    Show-CollectedData
+    
+    # Display All Tested Data
+    Show-TestedData
+
+    # Display Fixes
 }
