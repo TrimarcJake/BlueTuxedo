bb5634402f2d8d5a7f11164072ea75a7a9458324
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-10-14 14:00:57 -0500

    Fixed Empty Zone Scope Container test.

--- Private/Test/Test-ZoneScopeContainer.ps1
+++ Private/Test/Test-ZoneScopeContainer.ps1
@@ -10,13 +10,13 @@ function Test-ZoneScopeContainer {
     foreach ($zoneScopeContainer in $ZoneScopeContainers) {
         $isEmpty = $true
         if ( (Get-ADObject -Filter * -SearchBase $zoneScopeContainer.'Zone Scope Container DN' -Server $zoneScopeContainer.Domain).Count -gt 0) {
-            $isEmpty = $false
-        }
-        $AddToList = [PSCustomObject]@{
-            'Domain'    = $zoneScopeContainer.Domain
-            'Zone Name' = $zoneScopeContainer.'Zone Name'
-            'Zone Type' = $zoneScopeContainer.'Zone Scope Container DN'
-            'Is Empty?' = $isEmpty
+            break
+        } else {
+            $AddToList = [PSCustomObject]@{
+                'Domain'    = $zoneScopeContainer.Domain
+                'Zone Name' = $zoneScopeContainer.'Zone Name'
+                'Zone Type' = $zoneScopeContainer.'Zone Scope Container DN'
+            }
         }
 
         $FailedZoneScopeContainerList += $AddToList
