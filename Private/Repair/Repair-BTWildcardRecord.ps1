8b4f4a6512eb171ca78195137c27d46a56c3b060
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-10-15 08:23:34 -0500

    Not working, but kid needs food.

--- Private/Repair/Repair-BTWildcardRecord.ps1
+++ Private/Repair/Repair-BTWildcardRecord.ps1
@@ -2,6 +2,39 @@ function Repair-BTWildcardRecord {
     [CmdletBinding()]
     param (
         [Parameter()]
-        [TypeName]$ParameterName
+        [array]$WildcardRecords,
+        [switch]$Run = $false
     )
+
+    if ($null -eq $WildcardRecords) {
+        $WildcardRecords = Test-BTWildcardRecord
+    }
+
+    if ($Run) {
+        foreach ($wildcardrecord in $WildcardRecords) {
+            $type = "-$($wildcardrecord.'Correct Type')"
+            if ($wildcardrecord.'Wildcard Exists?' -ne 'N/A') {
+                Remove-DnsServerResourceRecord -ZoneName $wildcardrecord.Domain -ComputerName $wildcardrecord.Domain -Name '*'
+            }
+            Add-DnsServerResourceRecord -ZoneName $wildcardrecord.Domain -ComputerName $wildcardrecord.Domain $type -Name '*' -IPv4Address '0.0.0.0'
+        }
+    } else {
+        foreach ($wildcardrecord in $WildcardRecords) {
+            $type = "-$($wildcardrecord.'Correct Type')"
+            if ($wildcardrecord.'Wildcard Exists?' -ne 'N/A') {
+                Write-Host "Run the following code block to delete the Wildcard Record of incorrect type ($($wildcardrecord.'Current Wildcard Type')) and replace with a Wildcard Record of the correct type ($type) in the $($wildcardrecord.Domain) domain" -ForegroundColor Green
+                Write-Host @"
+Remove-DnsServerResourceRecord -ZoneName $($wildcardrecord.Domain) -ComputerName $($wildcardrecord.Domain) -Name '*'
+Add-DnsServerResourceRecord -ZoneName $($wildcardrecord.Domain) -ComputerName $($wildcardrecord.Domain) $type -Name '*' -IPv4Address '0.0.0.0'
+              
+"@
+            } else {
+                Write-Host "Run the following code block to create a Wildcard Record in the $($wildcardrecord.Domain) domain" -ForegroundColor Green
+                Write-Host @"
+Add-DnsServerResourceRecord -ZoneName $($wildcardrecord.Domain) -ComputerName $($wildcardrecord.Domain) $type -Name '*' -IPv4Address '0.0.0.0'
+              
+"@
+            }
+        }
+    }
 }
