1d2af1ef4f297f0e0808dec099390cb98dbf5f6f
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-10-15 07:55:47 -0500

    Working as intended.

--- Private/Repair/Repair-BTTombstonedNode.ps1
+++ Private/Repair/Repair-BTTombstonedNode.ps1
@@ -2,6 +2,25 @@ function Repair-BTTombstonedNode {
     [CmdletBinding()]
     param (
         [Parameter()]
-        [TypeName]$ParameterName
+        [array]$TombstonedNodes,
+        [switch]$Run = $false
     )
+
+    if ($null -eq $TombstonedNodes) {
+        $TombstonedNodes = Get-BTTombstonedNode
+    }
+
+    if ($Run) {
+        foreach ($tombstonednode in $TombstonedNodes) {
+            Remove-ADObject $tombstonednode.'Node DN'
+        }
+    } else {
+        foreach ($tombstonednode in $TombstonedNodes) {
+            Write-Host "Run the following code block to delete the $($tombstonednode.'Node Name') node from the $($tombstonednode.'Zonee Name') zone." -ForegroundColor Green
+            Write-Host @"
+Remove-ADObject '$($tombstonednode.'Node DN')'
+
+"@
+        }
+    }
 }
