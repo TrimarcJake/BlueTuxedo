d26c75bf5b69e31cf5841030c14e2c9a3e59ee50
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-10-15 20:26:34 -0500

    Commented so Build script stops complaining.

--- Private/Repair/Repair-Template.ps1
+++ Private/Repair/Repair-Template.ps1
@@ -1,26 +1,26 @@
-function Repair-BTThing {
-    [CmdletBinding()]
-    param (
-        [Parameter()]
-        [array]$Things,
-        [switch]$Run = $false
-    )
+# function Repair-BTThing {
+#     [CmdletBinding()]
+#     param (
+#         [Parameter()]
+#         [array]$Things,
+#         [switch]$Run = $false
+#     )
 
-    if ($null -eq $Things) {
-        $Things = Get-BTThing
-    }
+#     if ($null -eq $Things) {
+#         $Things = Get-BTThing
+#     }
 
-    if ($Run) {
-        foreach ($thing in $Things) {
+#     if ($Run) {
+#         foreach ($thing in $Things) {
             
-        }
-    } else {
-        foreach ($thing in $Things) {
-            Write-Host "DESCRIPTION OF CODE BLOCK" -ForegroundColor Green
-            Write-Host @"
-CODE BLOCK
+#         }
+#     } else {
+#         foreach ($thing in $Things) {
+#             Write-Host "DESCRIPTION OF CODE BLOCK" -ForegroundColor Green
+#             Write-Host @"
+# CODE BLOCK
 
-"@
-        }
-    }
-}
+# "@
+#         }
+#     }
+# }
