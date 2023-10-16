f07c2a787d50fb8f91b3a2e16a4ac962bfb4e8bb
TrimarcJake <jakehildreth@trimarcsecurity.com>
2023-10-15 21:02:54 -0500

    Working correctly finally.

--- Private/Repair/Repair-BTWildcardRecord.ps1
+++ Private/Repair/Repair-BTWildcardRecord.ps1
@@ -16,24 +16,37 @@ function Repair-BTWildcardRecord {
             if ($wildcardrecord.'Wildcard Exists?') {
                 Remove-DnsServerResourceRecord -ComputerName $wildcardrecord.Domain -ZoneName $wildcardrecord.Domain -RRType $wildcardrecord.'Current Wildcard Type' -Name '*'
             }
-            Add-DnsServerResourceRecord -ComputerName $wildcardrecord.Domain -ZoneName $wildcardrecord.Domain $type -Name '*' -IPv4Address '0.0.0.0'
+            if ($type -eq '-Txt') {
+                $AddWildcardScriptBlock = "Add-DnsServerResourceRecord -ComputerName $($wildcardrecord.Domain) -ZoneName $($wildcardrecord.Domain) $type -Name '*' -DescriptiveText '0.0.0.0'"
+            } elseif ($type -eq '-A') {
+                $AddWildcardScriptBlock = "Add-DnsServerResourceRecord -ComputerName $($wildcardrecord.Domain) -ZoneName $($wildcardrecord.Domain) $type -Name '*' -RecordData '0.0.0.0'"
+            }
+            $ScriptBlock = [scriptblock]::Create($AddWildcardScriptBlock)
+            Invoke-Command -ScriptBlock $ScriptBlock
         }
     } else {
         foreach ($wildcardrecord in $WildcardRecords) {
             $type = "-$($wildcardrecord.'Correct Type')"
             if ($wildcardrecord.'Wildcard Exists?') {
                 Write-Host "Run the following code block to delete the Wildcard Record of incorrect type ($($wildcardrecord.'Current Wildcard Type')) and replace with a Wildcard Record of the correct type ($type) in the $($wildcardrecord.Domain) domain" -ForegroundColor Green
+                if ($type -eq '-Txt') {
+                    $AddWildcardScriptBlock = "Add-DnsServerResourceRecord -ComputerName $($wildcardrecord.Domain) -ZoneName $($wildcardrecord.Domain) $type -Name '*' -DescriptiveText '0.0.0.0'"
+                } elseif ($type -eq '-A') {
+                    $AddWildcardScriptBlock = "Add-DnsServerResourceRecord -ComputerName $($wildcardrecord.Domain) -ZoneName $($wildcardrecord.Domain) $type -Name '*' -RecordData '0.0.0.0'"
+                }
                 Write-Host @"
 Remove-DnsServerResourceRecord -ComputerName $($wildcardrecord.Domain) -ZoneName $($wildcardrecord.Domain) -RRType $($wildcardrecord.'Current Wildcard Type') -Name '*'
-Add-DnsServerResourceRecord -ComputerName $($wildcardrecord.Domain) -ZoneName $($wildcardrecord.Domain) $type -Name '*' -IPv4Address '0.0.0.0'
+$AddWildcardScriptBlock
               
 "@
             } else {
                 Write-Host "Run the following code block to create a Wildcard Record in the $($wildcardrecord.Domain) domain" -ForegroundColor Green
-                Write-Host @"
-Add-DnsServerResourceRecord -ZoneName $($wildcardrecord.Domain) -ComputerName $($wildcardrecord.Domain) $type -Name '*' -IPv4Address '0.0.0.0'
-              
-"@
+                if ($type -eq '-Txt') {
+                    Write-Host "Add-DnsServerResourceRecord -ComputerName $($wildcardrecord.Domain) -ZoneName $($wildcardrecord.Domain) $type -Name '*' -DescriptiveText '0.0.0.0'"
+                } elseif ($type -eq '-A') {
+                    Write-Host "Add-DnsServerResourceRecord -ComputerName $($wildcardrecord.Domain) -ZoneName $($wildcardrecord.Domain) $type -Name '*' -RecordData '0.0.0.0'"
+                }
+                Write-Host
             }
         }
     }
